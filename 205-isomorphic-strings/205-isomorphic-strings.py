33c3f450f8c80e578a7b470dae0a8b16cf6f2bbf
Josua Naiborhu <naiborhujosua@users.noreply.github.com>
2022-03-25 23:10:24 +0700

    Time: 59 ms (52.48%), Space: 14.3 MB (52.90%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        n = len(s)
+        m = len(t)
+        if n != m:
+            return False
+        dic = {}
+        for i in range(n):
+            if s[i] in dic:
+                if dic[s[i]] != t[i]:
+                    return False
+            elif t[i] in dic.values():
+                return False
+            else:
+                dic[s[i]] = t[i]
+        return True
