af95f91f5aee64c2f189b6b3eec94fd9b400a868
Josua Naiborhu <naiborhujosua@users.noreply.github.com>
2022-03-26 14:32:32 +0700

    Time: 50 ms (42.22%), Space: 13.8 MB (65.37%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,17 +1,33 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        num_char = [str(num) for num in digits]
-        num_str = int("".join(num_char)) + 1
+        digitLen = len(digits)
+        allNines = True
+        for digit in digits:
+            if digit !=9:
+                allNines =False
+                break
+                
+        result = [None]*(digitLen +1) if allNines else [None]*digitLen
         
-        result =[]
-        for i in str(num_str):
-            result.append(int(i))
+        sum = digits[digitLen-1]+1
+        carry = sum//10
+        result[len(result)-1] = sum%10
+        
+        ptr1 = digitLen -2
+        ptr2 = len(result)-2
+        
+        while ptr1 >=0:
+            sum = digits[ptr1]+carry
+            carry = sum//10
+            result[ptr2]= sum%10
+            ptr1 -=1
+            ptr2 -=1
             
-        return result
-       
         
+        if allNines:
+            result[0]=1
         
-        
+        return result 
             
-            
+        
         
